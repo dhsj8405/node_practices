51af0d16b1f0043754304006034e1b04f8d58243
dhsj8405 <dhsj8405@naver.com>
2021-11-15 13:12:46 +0900

    test

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,10 @@
+/*
+douzone-dhsj8405-math 파일 모듈 테스트
+*/
+
+var dzMath = require('../douzone-dhsj8405-math/douzone-dhsj8405-math');
+
+console.log(dzMath.sum(10,20,30,40,50))
+console.log(dzMath.max(10,20,30,40,50))
+console.log(dzMath.min(10,20,30,40,50))
+
---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,14 @@
+/*
+douzone-dhsj8405-math npm 모듈 테스트(모듈 패키지: 로컬 배포)
+
+
+$ npm i ../douzone-bipa-mat
+명령으로 설치 후 테스트 할 것fdsfdasfsdafdasfads
+*/
+
+var dzMath = require('douzone-dhsj8405-math');
+
+console.log(dzMath.sum(10,20,30,40,50))
+console.log(dzMath.max(10,20,30,40,50))
+console.log(dzMath.min(10,20,30,40,50))
+
